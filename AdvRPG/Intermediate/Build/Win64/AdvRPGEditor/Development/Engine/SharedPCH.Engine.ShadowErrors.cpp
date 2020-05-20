d1d0d912771b59eb8f845d54ba2e8d4965e1dde8
SungHoon(PC) <ksh920610@gmail.com>
2020-05-20 20:28:42 +0900

    첫 프로젝트 생성

---
+++ AdvRPG/Intermediate/Build/Win64/AdvRPGEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "D:/Development/SourceTree/Unreal_AdvRPG/AdvRPG/Intermediate/Build/Win64/AdvRPGEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ AdvRPG/Source/AdvRPG/AdvRPGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvRPGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAdvRPGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAdvRPGGameMode();
+};
+
+
+
